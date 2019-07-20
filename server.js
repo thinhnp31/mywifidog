e4fe5434ed9f60b9dc90e5e96a96b9506d1548fe
thinhnp31 <42092351+thinhnp31@users.noreply.github.com>
2019-07-20 10:15:14 +0700

    v1.0

---
+++ server.js
@@ -0,0 +1,40 @@
+// server.js
+
+const express = require('express');
+const app = express();
+const port = 80;
+const crypto = require('crypto')
+app.listen(port, function(){
+  console.log('Node js Express js Tutorial');
+  
+});
+
+app.get("/", (req, res) => {
+  res.send("Hello World!")
+})
+
+app.get("/auth", (req, res) => {
+  console.log("Auth: ")
+  console.log(req.query);
+  res.send(JSON.stringify({auth: 1}));
+})
+
+app.get("/login", (req, res) => {
+  console.log("Login: ")
+  console.log(req.query);
+  res.sendFile("login.html", { root : __dirname});
+})
+
+app.get("/submit", (req, res) => {
+  console.log("Submit: ")
+  console.log(req.query);
+  let random = Math.random().toString();
+  let token = crypto.createHash('sha1').update(random).digest('hex');
+  res.redirect("http://1.2.3.4:2060/auth?token=" + token);
+})
+
+app.get("/portal", (req, res) => {
+  console.log("Auth: ")
+  console.log(req.query);
+  res.send("Success!");
+})
