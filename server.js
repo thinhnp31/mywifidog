dcdd39fd007b38fb429f599e20496dfdc1766e68
thinhnp31 <42092351+thinhnp31@users.noreply.github.com>
2019-09-06 00:06:12 +0700

    v1.1 updated 20190906

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const app = express();
 const port = 80;
 const crypto = require('crypto')
 app.listen(port, function(){
-  console.log('Node js Express js Tutorial');
+  console.log('Server is running on port ' + port + "...");
   
 });
 
@@ -22,16 +22,16 @@ app.get("/auth", (req, res) => {
 app.get("/login", (req, res) => {
   console.log("Login: ")
   console.log(req.query);
-  res.sendFile("login.html", { root : __dirname});
+  res.sendFile("./view/login.html", { root : __dirname});
 })
 
-app.get("/submit", (req, res) => {
-  console.log("Submit: ")
-  console.log(req.query);
-  let random = Math.random().toString();
-  let token = crypto.createHash('sha1').update(random).digest('hex');
-  res.redirect("http://1.2.3.4:2060/auth?token=" + token);
-})
+// app.get("/submit", (req, res) => {
+//   console.log("Submit: ")
+//   console.log(req.query);
+//   let random = Math.random().toString();
+//   let token = crypto.createHash('sha1').update(random).digest('hex');
+//   res.redirect("http://1.2.3.4:2060/auth?token=" + token);
+// })
 
 app.get("/portal", (req, res) => {
   console.log("Auth: ")
